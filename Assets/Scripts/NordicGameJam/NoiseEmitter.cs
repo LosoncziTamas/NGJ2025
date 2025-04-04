e2445e43f0e7fef7c1beb17757652ae983ee816a
Tamás Losonczi <losonczi.tamas91@gmail.com>
2025-04-04 22:00:21 +0200

    create noise emitter and related SO

---
+++ Assets/Scripts/NordicGameJam/NoiseEmitter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace NordicGameJam
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class NoiseEmitter : MonoBehaviour
+    {
+        [SerializeField] private AudioSource _audioSource;
+        [SerializeField] private NoiseProps _noiseProps;
+
+        private void OnValidate()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        public void EmitNoise()
+        {
+            _audioSource.PlayOneShot(_noiseProps.ClipToPlay, _noiseProps.Volume);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            // TODO: check layers
+            EmitNoise();
+        }
+        
+        private void OnCollisionEnter(Collision collision)
+        {
+            // TODO: check layers
+            EmitNoise();
+        }
+    }
+}
---
+++ Assets/Scripts/NordicGameJam/NoiseProps.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace NordicGameJam
+{
+    [CreateAssetMenu]
+    public class NoiseProps : ScriptableObject
+    {
+        [Range(0, 10)]
+        public float NoiseLevel;
+        public float Volume;
+        public AudioClip ClipToPlay;
+    }
+}
