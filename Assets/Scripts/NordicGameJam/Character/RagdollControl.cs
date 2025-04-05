9287b7098a8eb9008ff1cdfa089ae74460593421
Tamás Losonczi <losonczi.tamas91@gmail.com>
2025-04-05 14:50:32 +0200

    add RagdollControl.cs skeleton

---
+++ Assets/Scripts/NordicGameJam/Character/RagdollControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace NordicGameJam.Character
+{
+    public class RagdollControl : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _leftLeg;
+        [SerializeField] private Rigidbody _rightLeg;
+        [SerializeField] private Rigidbody _hip;
+
+        private Vector3 _originalPosition;
+        private Quaternion _originalOrientation;
+        
+        [SerializeField] private float _speed;
+
+        private void Start()
+        {
+            _originalPosition = _hip.transform.localPosition;
+            _originalOrientation = _hip.transform.localRotation;
+        }
+    }
+}
