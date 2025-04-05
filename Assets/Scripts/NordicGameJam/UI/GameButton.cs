c9f5bda9852608ca0c1a2f63dc75733140747d9e
Tamás Losonczi <losonczi.tamas91@gmail.com>
2025-04-05 17:10:52 +0200

    add game button with anim and audio click

---
+++ Assets/Scripts/NordicGameJam/UI/GameButton.cs
@@ -0,0 +1,49 @@
+﻿using DG.Tweening;
+using NordicGameJam.Audio;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace NordicGameJam.UI
+{
+    [RequireComponent(typeof(Button))]
+    public class GameButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        private Button _button;
+
+        [Header("Anim properties")] 
+        [SerializeField] private float _scale = 1.2f;
+        [SerializeField] private float _duration = 0.4f;
+        [SerializeField] private Ease _ease = Ease.InOutSine;
+
+        private void Awake()
+        {
+            _button = GetComponent<Button>();
+        }
+
+        private void OnEnable()
+        {
+            _button.onClick.AddListener(OnButtonClick);
+        }
+
+        private void OnDisable()
+        {
+            _button.onClick.RemoveListener(OnButtonClick);
+        }
+        
+        private void OnButtonClick()
+        {
+            SimpleAudioManager.Instance.PlayClick();
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            _button.transform.DOScale(Vector3.one * _scale, _duration).SetEase(_ease);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            _button.transform.DOScale(Vector3.one, _duration).SetEase(_ease);
+        }
+    }
+}
