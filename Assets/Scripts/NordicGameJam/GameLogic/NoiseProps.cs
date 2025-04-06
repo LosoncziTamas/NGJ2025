af677889b2f5a85c344486a3c59ed13d4fc7e076
Tamás Losonczi <losonczi.tamas91@gmail.com>
2025-04-06 11:16:23 +0200

    add rubber duck prop

--- Assets/Scripts/NordicGameJam/GameLogic/NoiseProps.cs
+++ Assets/Scripts/NordicGameJam/GameLogic/NoiseProps.cs
@@ -5,7 +5,7 @@ namespace NordicGameJam.GameLogic
     [CreateAssetMenu]
     public class NoiseProps : ScriptableObject
     {
-        [Range(0, 10)]
+        [Range(0, 100)]
         public float NoiseLevel;
         public float Volume;
         public AudioClip ClipToPlay;
